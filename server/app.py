25d4a43c738270e9f0f32e7c3ade248a7260a61c
Samuel <duckbutt@Samuels-Mac-Studio.local>
2025-11-18 02:43:21 -0800

    first commit

--- server/app.py
+++ server/app.py
@@ -1,1 +1,22 @@
-existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+existing_models = ["Beedle", "Crossroads", "M2", "Panique"]
+
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Welcome to Flatiron Cars"
+
+
+@app.route("/<model>", methods=["GET"])
+def get_model(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
